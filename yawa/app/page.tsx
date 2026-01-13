755201023b633b89f235a78b53b026f230d1f7bd
Logan Bjork <loganbjork@ufl.edu>
2026-01-13 08:14:36 -0500

    i just made some BULLSHIT!!!!!!!!!!!!

--- yawa/app/page.tsx
+++ yawa/app/page.tsx
@@ -1,11 +1,17 @@
-import { OAuthSpotify } from "@/components/OAuthSpotify";
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET!;
+const redirectUri = 'http://127.0.0.1:8080';
+
+// auth to use API
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center text-zinc-50 font-sans bg-stone-900">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-stone-900 sm:items-start">
-        <OAuthSpotify></OAuthSpotify>
+      
+      
       </main>
     </div>
   );
